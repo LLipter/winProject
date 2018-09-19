4fdbe3fcc947f77aa8c60d3d818945714935f37f
LLipter <katsudonn@163.com>
2018-09-20 00:16:55 +0800

    invoke method through reflection dynamically

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using CSharpLibrary;
 
 namespace Client
@@ -7,10 +8,28 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Invoke method in CSMath library!");
+            Console.WriteLine("Invoke method in CSMath library by adding reference!");
             int[] array = { 1, 2, 3, 4, 5 };
             int sum = CSMath.sum(array);
             Console.WriteLine("Sum of array = {0}", sum);
+
+            Console.WriteLine("Invoke method in CSMath library by loading dynamically!");
+            Assembly a = Assembly.LoadFrom("../../CSharpLibrary/CSharpLibrary/bin/debug/netstandard2.0/CSharpLibrary.dll");
+
+
+
+
+            Type lib = a.GetType("CSharpLibrary.CSMath");
+        
+            MethodInfo mi = lib.GetMethod("sum");
+            object o = Activator.CreateInstance(lib);
+            Object[] parameters = new Object[1];
+            parameters[0] = array;
+            sum = (int) mi.Invoke(o, parameters);
+            Console.WriteLine("Sum of array = {0}", sum);
+
+
+            
         }
     }
 }
