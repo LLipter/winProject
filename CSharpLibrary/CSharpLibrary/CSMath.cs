fdcec4c1f47075b4d3739b029de279e750470250
LLipter <katsudonn@163.com>
2018-09-19 23:43:43 +0800

    cslibrary

---
+++ CSharpLibrary/CSharpLibrary/CSMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpLibrary
+{
+    public class CSMath
+    {
+        public static int max(int a,int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public static int min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        public static int sum(int[] array)
+        {
+            int result = 0;
+            foreach(int i in array)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+    }
+}
