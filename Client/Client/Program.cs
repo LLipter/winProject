8c877ceb8e1cb2dff9ad7fac0899517cee7563d0
LLipter <katsudonn@163.com>
2018-09-20 22:21:23 +0800

    invoke cpp library from c#

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using CSharpLibrary;
 
 namespace Client
 {
     class Program
     {
+        [DllImport("../../CppLibrary/x64/Release/CppLibrary.dll", EntryPoint = "getMax")]
+        public static extern int getMax(int a, int b);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Invoke method in CSMath library by adding reference!");
+            Console.WriteLine("Invoke method in C# library by adding reference!");
             int[] array = { 1, 2, 3, 4, 5 };
             int sum = CSMath.sum(array);
             Console.WriteLine("Sum of array = {0}", sum);
 
-            Console.WriteLine("Invoke method in CSMath library by loading dynamically!");
+            Console.WriteLine("Invoke method in C# library by loading dynamically!");
             Assembly a = Assembly.LoadFrom("../../CSharpLibrary/CSharpLibrary/bin/debug/netstandard2.0/CSharpLibrary.dll");
-
-
-
-
             Type lib = a.GetType("CSharpLibrary.CSMath");
-        
             MethodInfo mi = lib.GetMethod("sum");
             object o = Activator.CreateInstance(lib);
             Object[] parameters = new Object[1];
@@ -29,7 +28,12 @@ namespace Client
             Console.WriteLine("Sum of array = {0}", sum);
 
 
-            
+
+            Console.WriteLine("Invoke method in C++ library!");
+            Console.WriteLine(getMax(1, 2));
+
+
+
         }
     }
 }
