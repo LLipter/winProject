0ecae3338ca636b8a67369ec0cd20e6eb9510d50
LLipter <katsudonn@163.com>
2018-10-10 18:06:04 +0800

    create and save word file through code

--- 04_COM/Word/Word/Program.cs
+++ 04_COM/Word/Word/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
 
 namespace Word
 {
@@ -10,6 +12,24 @@ namespace Word
     {
         static void Main(string[] args)
         {
+            string filePath = Environment.CurrentDirectory + "\\test.docx";
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+
+            Application app = new Application();
+            Document doc = app.Documents.Add();
+
+
+
+
+
+
+
+
+
+            doc.SaveAs2(filePath);
+            app.Quit();
+
         }
     }
 }
