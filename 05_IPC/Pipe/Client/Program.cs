859c0c9cfa7688498b4e22c41953dfe828d951a1
LLipter <katsudonn@163.com>
2018-10-17 15:04:30 +0800

    IPC pipe

---
+++ 05_IPC/Pipe/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "testpipe", PipeDirection.Out);
+            pipeClient.Connect();
+            Console.WriteLine("Connection built");
+            StreamWriter sw = new StreamWriter(pipeClient);
+            sw.WriteLine("Hello, I'm LLipter!");
+            sw.Flush();
+            sw.Close();
+            Console.WriteLine("Connection closed");
+        }
+    }
+}
---
+++ 05_IPC/Pipe/Server/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+            while (true)
+            {
+                NamedPipeServerStream pipServer = new NamedPipeServerStream("testpipe", PipeDirection.In);
+                Console.WriteLine("Wait for connection");
+                pipServer.WaitForConnection();
+                Console.WriteLine("Connection built");
+                StreamReader sr = new StreamReader(pipServer);
+                string msg = sr.ReadToEnd();
+                Console.WriteLine(msg);
+                pipServer.Close();
+            }
+        }
+    }
+}
