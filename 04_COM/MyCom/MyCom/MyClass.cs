de8365fa13c8e9b45c0b4b1e9e70c5ef4b0a0ed1
LLipter <katsudonn@163.com>
2018-10-17 12:35:01 +0800

    MyCom

---
+++ 04_COM/MyCom/MyCom/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCom
+{
+
+    [ComVisible(true)]
+    [Guid("48CF4385-5FC5-4DA2-9D1F-43EECBE754D5")]
+    public interface IMyClass
+    {
+        int Add(int x, int y);
+    }
+
+
+    [ComVisible(true)]
+    [Guid("EAFD6DD6-E49A-480D-A57D-43A38132A252")]
+    [ProgId("MyCom.MyClass")]
+    public class MyClass : IMyClass
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }  
+
+}
