2cef1db0b47312f8622900acf9b116a23147fffc
LLipter <katsudonn@163.com>
2018-10-17 13:29:48 +0800

    invoke in C# through Activator

---
+++ 04_COM/CsClient/CsClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace CsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Guid guid = new Guid("EAFD6DD6-E49A-480D-A57D-43A38132A252");
+            Type type = Type.GetTypeFromCLSID(guid);
+            object obj = Activator.CreateInstance(type);
+            object[] para = new object[] { 1, 2 };
+            int result = (int)type.InvokeMember("Add",BindingFlags.InvokeMethod, null, obj, para);
+            Console.WriteLine(result);
+        }
+    }
+}
--- 04_COM/CsClient/CsClient/Properties/AssemblyInfo.cs
+++ 04_COM/CsClient/CsClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Client")]
+[assembly: AssemblyTitle("CsClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Client")]
+[assembly: AssemblyProduct("CsClient")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("7c294398-0875-4034-ae87-ab9c9306cd60")]
+[assembly: Guid("9c2e1925-6390-4020-b8ff-a2e197670f57")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
