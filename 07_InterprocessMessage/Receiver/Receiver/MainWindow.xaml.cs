6c9afc8e68bbb0c3f67b4f2854cb72ebe09d76e0
LLipter <katsudonn@163.com>
2018-11-02 23:09:59 +0800

    try again, and failed again

--- 07_InterprocessMessage/Receiver/Receiver/MainWindow.xaml.cs
+++ 07_InterprocessMessage/Receiver/Receiver/MainWindow.xaml.cs
@@ -40,24 +40,64 @@ namespace Receiver
             }
         }
 
+        private byte[] bytes;
+        private int index;
+
         IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             Console.WriteLine(msg.ToString());
             if (msg == WM_COPYDATA)
             {
-                
+                // Solution 1 (failed to pass message larger than 8 bytes)
+                /*
                 COPYDATASTRUCT data = (COPYDATASTRUCT)Marshal.PtrToStructure(lParam, typeof(COPYDATASTRUCT)); 
                 // MessageBox.Show(wParam.ToString());
                 string str = Encoding.Default.GetString(data.data,0,(int)wParam);
                 MessageBox.Show(str);
                 lblMsg.Content = "Received Message: " + str;
-                
+                */
 
+                // Solution 2 (failed to pass message larger than 4 bytes)
                 /*
                 string data = Marshal.PtrToStringAnsi(lParam);
                 lblMsg.Content = "Received Message: " + data;
                 MessageBox.Show(data);
                 */
+
+                // Solution 3 (also failed to pass message larger than 4 bytes)
+                /*
+                byte[] bytes = new Byte[(int)wParam];
+                Marshal.Copy(lParam, bytes, 0, (int)wParam);
+                string data = Encoding.Default.GetString(bytes);
+                lblMsg.Content = "Received Message: " + data;
+                MessageBox.Show(data);
+                */
+
+                // Solution 4, try solution 1-3 multiplu times. Only pass a small piece of message in a single message
+                // failed again, and I don't know why
+                /*
+                // signal of starting transmitting
+                if (wParam == (IntPtr)999 && lParam != IntPtr.Zero)
+                {
+                    bytes = new byte[(int)lParam];
+                    index = 0;
+                }
+                // signal of ending transmitting
+                else if (wParam == (IntPtr)999 && lParam == (IntPtr)999)
+                {
+                    string data = Encoding.Default.GetString(bytes);
+                    lblMsg.Content = "Received Message: " + data;
+                    MessageBox.Show(data);
+                }
+                else
+                {
+                    Marshal.Copy(lParam, bytes, index, (int)wParam);
+                    index += (int)wParam;
+                }
+                */
+
+
+
             }
             return hwnd;
 
--- 07_InterprocessMessage/Sender/Sender/MainWindow.xaml.cs
+++ 07_InterprocessMessage/Sender/Sender/MainWindow.xaml.cs
@@ -44,7 +44,9 @@ namespace Sender
                 return;
             }
             // MessageBox.Show(IntPtr.Size.ToString());
-            
+
+            // Solution 1 (failed to pass message larger than 8 bytes)
+            /*
             COPYDATASTRUCT data = new COPYDATASTRUCT();
             byte[] bytes = Encoding.Default.GetBytes(txtMsg.Text);
             // MessageBox.Show(bytes.Length.ToString());
@@ -58,12 +60,52 @@ namespace Sender
             Marshal.StructureToPtr(data, ptr, true);
             int res = SendMessage(hWnd, WM_COPYDATA, new IntPtr(bytes.Length), ptr);
             // MessageBox.Show(res.ToString());
-            
+            Marshal.FreeHGlobal(ptr);
+            */
 
+
+            // Solution 2 (failed to pass message larger than 4 bytes)
             /*
             int res = SendMessage(hWnd, WM_COPYDATA, new IntPtr(0), Marshal.StringToHGlobalAnsi(txtMsg.Text));
             */
 
+            // Solution 3 (also failed to pass message larger than 4 bytes)
+            /*
+            byte[] bytes = Encoding.Default.GetBytes(txtMsg.Text);
+            IntPtr ptr = Marshal.AllocHGlobal(bytes.Length);
+            Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            SendMessage(hWnd, WM_COPYDATA, (IntPtr)bytes.Length, ptr);
+            Marshal.FreeHGlobal(ptr);
+            */
+
+            // Solution 4, try solution 1-3 multiplu times. Only pass a small piece of message in a single message
+            // failed again, and I don't know why
+            /*
+            byte[] bytes = Encoding.Default.GetBytes(txtMsg.Text);
+            // signal of starting transmitting
+            SendMessage(hWnd, WM_COPYDATA, (IntPtr)999, (IntPtr)bytes.Length);
+            int index = 0;
+            while(index < bytes.Length)
+            {
+                IntPtr ptr = Marshal.AllocHGlobal(4);
+                if (index + 4 < bytes.Length)
+                {
+                    Marshal.Copy(bytes, index, ptr, 4);
+                    SendMessage(hWnd, WM_COPYDATA, (IntPtr)4, ptr);
+                    index += 4;
+                }
+                else
+                {
+                    int diff = bytes.Length - index;
+                    Marshal.Copy(bytes, index, ptr, diff);
+                    SendMessage(hWnd, WM_COPYDATA, (IntPtr)diff, ptr);
+                    index += diff;
+                }
+                Marshal.FreeHGlobal(ptr);
+            }
+            // signal of ending transmitting
+            SendMessage(hWnd, WM_COPYDATA, (IntPtr)999, (IntPtr)999);
+           */
         }
     }
 
